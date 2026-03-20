43c40c3b731c31e81e4ebbfce5038159b18e1efd
johan <johan@sta.co.id>
2026-03-20 16:01:57 +0700

    update

--- src/components/BannerSection/Banner/Banner.tsx
+++ src/components/BannerSection/Banner/Banner.tsx
@@ -1,47 +1,125 @@
-/* eslint-disable react/jsx-no-undef */
-//import React, { useContext } from "react";
-//import BannerSingle from "../BannerSingle/BannerSingle";
-//import { MasterContext } from "@/context/MasterContext";
-import Image from "next/image";
+"use client";
+
+import { useContext, useEffect, useState } from "react";
+import { MasterContext } from "@/context/MasterContext";
+import SingleBanner from "@/components/BannerSection/SingleBanner/SingleBanner";
+
 const Banner = () => {
-/*   const { movies } = useContext(MasterContext)
-  console.log(movies) */
-  
+  const { movies } = useContext(MasterContext);
+
+  const [currentItem, setCurrentItem] = useState(1);
+  const [progress, setProgress] = useState(0);
+  const [countdown, setCountdown] = useState(5);
+  //console.log("movies:", movies);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(0);
+      setCountdown(5);
+      setCurrentItem((prevItem) => (prevItem === 6 ? 1 : prevItem + 1));
+    }, 5000); // Switch image every 5 seconds
+
+    const progressInterval = setInterval(() => {
+      setProgress((prev) => prev + 1);
+      setCountdown((prev) => (prev > 1 ? prev - 0.05 : prev)); // Decrease countdown
+    }, 50);
+
+    // Cleanup function
+    return () => {
+      clearInterval(interval);
+      clearInterval(progressInterval);
+    };
+  }, [currentItem]);
+
   return (
-    <>
-      {/* <div className="carousel w-full">
-        {movies.map((movie)=> {
+    <div className="relative w-full">
+      <div className="carousel  w-full">
+        {movies.map((movie, index) => {
           return (
-            <BannerSingle movie={movie} key={movie.id}/>
-          )
+            <SingleBanner
+              key={movie.id}
+              movie={movie}
+              currentItem={currentItem}
+              itemIndex={index + 1}
+            />
+          );
         })}
-      </div> */}
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <Image
-            width={500}
-            height={100}
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
-            alt=""
-          />
-          </div>
-    </div>
-      <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-        <a href="#item4" className="btn btn-xs">
-          4
-        </a>
       </div>
-    </>
+
+      {/* buttons for the slider */}
+      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex justify-center gap-2 py-2  w-full">
+        <button
+          onClick={() => setCurrentItem(1)}
+          className={`btn btn-xs ${currentItem === 1 ? "btn-active" : ""}`}
+        >
+          1
+        </button>
+        <button
+          onClick={() => setCurrentItem(2)}
+          className={`btn btn-xs ${currentItem === 2 ? "btn-active" : ""}`}
+        >
+          2
+        </button>
+        <button
+          onClick={() => setCurrentItem(3)}
+          className={`btn btn-xs ${currentItem === 3 ? "btn-active" : ""}`}
+        >
+          3
+        </button>
+        <button
+          onClick={() => setCurrentItem(4)}
+          className={`btn btn-xs ${currentItem === 4 ? "btn-active" : ""}`}
+        >
+          4
+        </button>
+        <button
+          onClick={() => setCurrentItem(5)}
+          className={`btn btn-xs ${currentItem === 5 ? "btn-active" : ""}`}
+        >
+          5
+        </button>
+        <button
+          onClick={() => setCurrentItem(6)}
+          className={`btn btn-xs ${currentItem === 6 ? "btn-active" : ""}`}
+        >
+          6
+        </button>
+      </div>
+
+      {/* Circular progress bar with countdown */}
+      <div className="absolute top-6 right-4">
+        <svg className="w-12 h-12">
+          <circle
+            className="text-[#5a5a5a]"
+            strokeWidth="4"
+            stroke="currentColor"
+            fill="transparent"
+            r="18"
+            cx="24"
+            cy="24"
+          />
+          <circle
+            className="text-[white]"
+            strokeWidth="4"
+            strokeDasharray="113"
+            strokeDashoffset={113 - (113 * progress) / 100}
+            strokeLinecap="round"
+            stroke="currentColor"
+            fill="transparent"
+            r="18"
+            cx="24"
+            cy="24"
+          />
+          <text
+            x="24"
+            y="28"
+            textAnchor="middle"
+            className="text-sm font-bold fill-current text-[white]"
+          >
+            {Math.ceil(countdown)}
+          </text>
+        </svg>
+      </div>
+    </div>
   );
 };
 
--- src/context/MasterContext.tsx
+++ src/context/MasterContext.tsx
@@ -1,67 +1,184 @@
-"use client";
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { MasterDataContext } from '@/types/MasterContextType';
-import React, { ReactNode, useState,createContext, Children,useEffect } from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MasterDataContext } from "@/types/MasterContextType";
+import React, { createContext, ReactNode, useEffect, useState, useRef } from "react";
 
-//Context = cara share data ke banyak komponen tanpa props
-  const defaultContextValue: MasterDataContext = {
-    movies: [],
+const defaultContextValue: MasterDataContext = {
+  movies: [],
+  //searchResult: [],
+  //sliderData: [],
+  //similarMovies: [],
+  //loading: true,
+  //error: null,
+  //page: 1,
+  //detailsType: "movie",
+  //movieOrTv: "movie",
+  //singleMovie: {},
+  //movieId: "",
+  //setPage: () => {},
+  //setQuery: () => {},
+  //setSingleMovie: () => {},
+  //setMovieOrTv: () => {},
+  //setTrendingOptions: () => {},
+  //setDetailsType: () => {},
+  //setMovieId: () => {},
+};
 
-  }
 export const MasterContext = createContext<MasterDataContext>(defaultContextValue);
 
-interface MasterContextProp {
+interface BannerContextProps {
   children: ReactNode;
 }
 
 const API_KEY = "c7cf1258a5aa723e8a98f08f639e86b6";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-const MainContext: React.FC<MasterContextProp>= ({ children }) => {
+const MainContext: React.FC<BannerContextProps> = ({ children }) => {
+  //const [singleMovie, setSingleMovie] = useState<[{}]>({});
   const [movies, setMovies] = useState<any[]>([]);
-  const [error, setError] = useState<Error | null>(null);
+  const [searchResult, setSearchResult] = useState<any[]>([]);
+  const [sliderData, setSliderData] = useState<any[]>([]);
+  const [similarMovies, setSimilarMovies] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [page, setPage] = useState<number>(1);
+  const [query, setQuery] = useState<string>("");
   const [movieOrTv, setMovieOrTv] = useState<string>("movie");
   const [trendingOptions, setTrendingOptions] = useState<string>("top_rated");
+  const [detailsType, setDetailsType] = useState<"movie" | "tv">("movie");
+  const [movieId, setMovieId] = useState<string>("");
 
-  useEffect( () => {
-      const fetchData = async () => {
-        setLoading(true)
-        try {
-          const response = await fetch(`${BASE_URL}/${movieOrTv}/${trendingOptions}?api_key=${API_KEY}`)
-          if (!response.ok) {
-            const errorText = await response.text();
-            console.error("Response Error:", errorText)
-            throw new Error(`Network response was not ok: ${response.statusText}`)
-          }
-        
+  const prevTrendingOptionsRef = useRef<string>(trendingOptions);
 
-          const data = await response.json()
-          setMovies(data.results)
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `${BASE_URL}/${movieOrTv}/${trendingOptions}?api_key=${API_KEY}&page=${page}`
+        );
+        if (!response.ok) {
+          const errorText = await response.text();
+          console.error("Response Error:", errorText);
+          throw new Error(`Network response was not ok: ${response.statusText}`);
         }
-        catch (error) {
-          setError(error as Error)  
-        }
-        finally {
-          setLoading(false)
-        }
-        
-      };
-    }, [movieOrTv, trendingOptions]);
   
-  //const response = await fetch(`${BASE_URL}/${movieOrTv}/${trendingOptions}?api_key=${API_KEY}`)
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        setError(error as Error);
+        console.error("Fetch error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // Check if trendingOptions has changed, if so, reset page to 1
+    if (prevTrendingOptionsRef.current !== trendingOptions) {
+      setPage(1);
+    }
+
+    // Update the previous trendingOptions ref
+    prevTrendingOptionsRef.current = trendingOptions;
+
+    fetchData();
+  }, [page, movieOrTv, trendingOptions]);
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setSliderData(data.results);
+      } catch (error) {
+        setError(error as Error);
+        console.error("Fetch error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setSearchResult(data.results);
+      } catch (error) {
+        setError(error as Error);
+        console.error("Fetch error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [query]);
+
+  useEffect(() => {
+    const fetchSimilarMovies = async () => {
+      if (movieId) {
+        try {
+          const response = await fetch(
+            `${BASE_URL}/movie/${movieId}/similar?api_key=${API_KEY}`
+          );
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          setSimilarMovies(data.results);
+        } catch (error) {
+          setError(error as Error);
+          console.error("Fetch error:", error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchSimilarMovies();
+  }, [movieId]);
+
   const contextValue: MasterDataContext = {
     movies,
-  }
+/*     sliderData,
+    searchResult,
+    similarMovies,
+    loading,
+    error,
+    setPage,
+    page,
+    setQuery,
+    singleMovie,
+    setSingleMovie,
+    setMovieOrTv,
+    setTrendingOptions,
+    detailsType,
+    setDetailsType,
+    movieId,
+    setMovieId,
+    movieOrTv, */
+  };
 
   return (
-      <MasterContext.Provider value={defaultContextValue}>
-          {children}
-      </MasterContext.Provider>
-     
-  )
-}
+    <MasterContext.Provider value={contextValue}>
+      {children}
+    </MasterContext.Provider>
+  );
+};
 
-export default MainContext
+export default MainContext;
